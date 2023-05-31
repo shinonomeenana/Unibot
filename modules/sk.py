761c24f64c3bfadde7da569dce3763965fb29db0
綿菓子ウニ <unijzlsx@gmail.com>
2023-05-31 21:34:12 +0800

    修复bug：分数线未指定时区

--- modules/sk.py
+++ modules/sk.py
@@ -468,13 +468,13 @@ def drawscoreline(targetid=None, targetrank=None, targetrank2=None, starttime=0,
     fig_size = (12, 8)
     fig, ax = plt.subplots(1, 1, figsize=fig_size)
     df_full.plot(ax=ax)
-    date_form = DateFormatter("%m-%d %H:%M")
+    date_form = DateFormatter("%m-%d %H:%M", tz=pytz.timezone('Asia/Shanghai'))
     ax.xaxis.set_major_formatter(date_form)
 
     # 将matplotlib日期数字转回日期格式
     df_full.index = pd.to_datetime(df_full.index, origin='unix', unit='D')
     # 找到每天0点的位置，并在这个位置绘制垂直线
-    for loc in DayLocator().tick_values(df_full.index.min(), df_full.index.max()):
+    for loc in DayLocator(tz=pytz.timezone('Asia/Shanghai')).tick_values(df_full.index.min(), df_full.index.max()):
         ax.axvline(loc, color='gray', linewidth=0.7, linestyle='--')
     # 将日期转回matplotlib日期数字格式，以便于显示
     df_full.index = date2num(df_full.index.to_pydatetime())
